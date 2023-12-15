c230412aef000c7f59c9070bddd25d48b0bb19b3
ankit00010 <111192702+ankit00010@users.noreply.github.com>
2023-12-15 14:54:36 +0530

    complete setup of a server with bmi api routes

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const connectionDB = require("./config/dbConnection");
+const bmiRoutes = require("./routes/bmiRoutes");
+const dotenv = require("dotenv").config();
+const app = express();
+connectionDB();    //Database connection
+
+
+
+
+app.use(express.json());
+
+
+
+
+const port = 3000;  //default port
+
+
+app.use("/api/user/bmi", bmiRoutes);
+//Listening port
+
+
+app.listen(port, () => {
+    console.log(`Server is running on port : ${port}`);
+})
