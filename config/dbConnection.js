82e27c218eed2a67766caa51342e0b5b2ff99d2d
ankit00010 <111192702+ankit00010@users.noreply.github.com>
2023-12-15 14:54:57 +0530

    Database connection using mongoose

---
+++ config/dbConnection.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+
+
+
+
+
+const connectionDB = async () => {
+
+
+    try {
+        const connect = await mongoose.connect(process.env.MONGO_URI);
+
+        console.log("Database is connected : ", connect.connection.name);
+    }
+    catch (err) {
+        console.log(err);
+        process.exit(1);
+
+    }
+}
+
+module.exports = connectionDB;
